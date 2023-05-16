9a5b7224c2d01552dc50e0c47c2e3b9daf819327
BALAJI <wbalaji2021@gmail.com>
2023-05-16 16:30:31 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tabs from './components/Tabs';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Tabs Example</h1>
+      <Tabs />
+    </div>
+  );
+};
+
+export default App;
